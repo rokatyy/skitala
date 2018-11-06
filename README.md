d44c82d7b737cbf4bd828fc86243cffcb1d27697
rokatyy <35141662+rokatyy@users.noreply.github.com>
2018-11-06 23:20:55 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
 # skitala
 Attack option not finished yet.
 USAGE:
-      encoding: -e <string> <n>
+      encoding: -e <string> -f <file> -n <n>
       
-      decoding: -d <string> <n>
+      decoding: -d <string> -f <file> -n <n>
       
-      attack: -a <string> <minimum n> <maximum n>
+      attack: -a <string> -f <file> -n <minimum n> -m <maximum n>
+For example:
+
+for encoding text from file:
+	./skitala_shifr -e -f 1.txt -n 3
+for decoding text from file:
+	./skitala_shifr -d -f 1.txt -n 3
+for encoding text from string:
+	./skitala_shifr -e "Hello" -n 3
+for decoding text from string:
+	./skitala_shifr -d "Hello" -n 3
+
+
--- skitala_shifr.py
+++ skitala_shifr.py
@@ -1,10 +1,27 @@
 import os,sys,math,enchant,numpy
 d = enchant.Dict("en_US") #dictionary
 #encoder=decoder
+def initial_n():
+	"""
+	returns parameter argv = '-n'
+	"""
+	return int(sys.argv[sys.argv.index('-n')+1])
+def initial_m():
+	"""
+	returns parameter argv = '-m'
+	"""
+	return int(sys.argv[sys.argv.index('-m')+1])
 def numbers(string,m):
+	"""
+	returns size of matrix and string length
+	"""
 	return int(math.fabs((len(string)-1) // m)) + 1,len(string)
 
 def encoder(string,m): 
+	"""
+	encode string with key (key must be integer) 
+	use: numbers
+	"""
 	n,k=numbers(string,m)
 	new_string=[' ']*k
 	for i in range(k):
@@ -12,6 +29,9 @@ def encoder(string,m):
 	return ''.join(new_string)
 
 def decoder(string,m):
+	"""
+	decode string with key (key must be integer) 
+	"""
 	n,k=numbers(string,m)
 	arr=[string[x:x+m] for x in range (0, k, m)]
 	new_arr=[];	new_str=''
@@ -24,8 +44,12 @@ def decoder(string,m):
 	return new_str
 	
 	
-#attack,expecting 2 numbers	
+
 def attack(string,mini,maxi):   
+	"""
+	attack with using decode function more than (maxi-mini) times
+	then check text with English dictionary and try to find the best variant
+	"""
 	print("Begin attack..")
 	d={}
 	for i in range(mini,maxi):
@@ -41,29 +65,39 @@ def attack(string,mini,maxi):
 			print("Find!!!")
 			return  ' '.join(new_string),i
 			
-		
-		#print(i,encoder(string,i),sep=': ')
+
 if __name__=="__main__":
-	if sys.argv[1]=='-e':
-		
+	if '-f' in sys.argv:
 		try:
-			print(encoder(sys.argv[2],int(sys.argv[3])))
+			string = open(sys.argv[sys.argv.index('-f')+1],'r').read()
+		except:
+			print("No file there")
+			exit(0)
+	else: 
+		try:
+			string = sys.argv[2]
+		except: 
+			pass
+	if sys.argv[1]=='-e':	
+		try:
+			n = initial_n()
+			print(encoder(string,n))
 		except:
 			print("Something wrong, try -h parameter")
 	elif sys.argv[1]=='-d':
 		try:
-			print(decoder(sys.argv[2],int(sys.argv[3])))
+			n = initial_n()
+			print(decoder(string,n))
 		except:
 			print("Something wrong, try -h parameter")
 	elif sys.argv[1]=='-a':
 		try:
-			string,key=(attack((sys.argv[2]),int(sys.argv[3]),int(sys.argv[4])))
+			n = initial_n()
+			m = initial_m()
+			string,key=(attack(string,n,m))
 			print("Key =",key)
 			print("your string: ",string)
 		except:
 			print("Something wrong, try -h parameter")
 	else:
-		print("USAGE:")
-		print("      encoding: -e <string> <n>")
-		print("      decoding: -d <string> <n>")
-		print("      attack: -a <string> <minimum n> <maximum n>")
+		print(open("README.md",'r').read())
